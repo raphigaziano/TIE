21fd80e08f17b4e5adeca73a5649b581c954d1fa
Raphaël Gaziano <r.gaziano@gmail.com>
2013-05-14 10:54:58 +0200

    Quick test refacto for functionnal tests

---
+++ tie/tests/tests_tie.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+""" TIE lib's functionnal tests. """
+import unittest
+
+class TestTIE(unittest.TestCase):
+    """ Global functionnal tests """
+    def test_dumy(self):
+        self.fail()

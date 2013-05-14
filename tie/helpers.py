839edb92fb54462b31560e8c2cd95c55af2b43b6
Raphaël Gaziano <r.gaziano@gmail.com>
2013-05-14 13:55:06 +0200

    get_single_group doc

--- tie/helpers.py
+++ tie/helpers.py
@@ -9,5 +9,9 @@ def get_single_group(match, key=1):
     """
     Return one and only one group from the passed match object.
     If no group was defined in the matched regexp, return the whole match.
+    :param match: A re.match object to extract the group from.
+    :param key: Optionnal key arg to get a specific group.
+      Can be either a list index or a string to get a named group.
+      Defaults to 1, to return the first group.
     """
     return match.group(key) if match.groups() else match.group(0)

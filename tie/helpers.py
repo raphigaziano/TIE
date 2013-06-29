733b7f314e51c144fc669ead1dcc43ddfd0d6331
raphigaziano <r.gaziano@gmail.com>
2013-06-30 01:55:17 +0200

    helpers doc

--- tie/helpers.py
+++ tie/helpers.py
@@ -17,8 +17,9 @@ def get_single_group(match, key=1):
     """
     Return one and only one group from the passed match object.
     If no group was defined in the matched regexp, return the whole match.
-    :param match: A re.match object to extract the group from.
-    :param key: Optionnal key arg to get a specific group.
+    params:
+    match: A re.match object to extract the group from.
+    key:   Optionnal key arg to get a specific group.
       Can be either a list index or a string to get a named group.
       Defaults to 1, to return the first group.
     """

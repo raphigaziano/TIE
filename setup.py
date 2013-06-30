3dab31a48d42771e8d3f54789f0eae1fb68c4d6c
raphigaziano <r.gaziano@gmail.com>
2013-06-30 13:35:48 +0200

    Preparing for release

--- setup.py
+++ setup.py
@@ -13,10 +13,11 @@ config = {
     'author_email': 'r.gaziano@gmail.com',
     'packages': ['tie', 'tie.tests'],
     'scripts': [], # any script in the bin directory
-    'url': None,
+    'url': 'https://pypi.python.org/pypi/TIE',
     'download_url': None,
-    'license': 'LICENSE.txt',
-    'description': 'TODO',
+    'license': 'LICENCE.txt',
+    'description': 'A minimalist template ENGINE allowing users to define '
+                   'their own simple templating languages.',
     'long_description': open('README.rst').read(),
     'install_requires': [
     ]
